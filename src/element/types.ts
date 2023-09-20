2293dcf3c3c26663744e9bb6bdf2814ec62f6b11
Jasper0077 <jasperhwong@hotmail.com>
2023-09-20 23:41:01 +0800

    update types

---
+++ src/element/types.ts
@@ -0,0 +1,46 @@
+import { Point } from "../types";
+
+type _BaseElement = Readonly<{
+  id: string;
+  x: number;
+  y: number;
+  strokeColor: string;
+  backgroundColor: string;
+  strokeWidth: number;
+  roughness: number;
+  opacity: number;
+  width: number;
+  height: number;
+  customData?: Record<string, any>;
+}>;
+
+export type SelectionElement = _BaseElement & {
+  type: "selection";
+};
+
+export type RectangleElement = _BaseElement & {
+  type: "rectangle";
+};
+
+export type DiamondElement = _BaseElement & {
+  type: "diamond";
+};
+
+export type EllipseElement = _BaseElement & {
+  type: "ellipse";
+};
+
+export type FreeDrawElement = _BaseElement & Readonly<{
+  type: "freedraw";
+  points: readonly Point[];
+}>;
+
+export type LinearElement = _BaseElement &
+  Readonly<{
+    type: "line" | "arrow";
+    points: readonly Point[];
+  }>;
+
+export type GenericElement = SelectionElement | RectangleElement | DiamondElement | EllipseElement;
+
+export type Element = GenericElement | FreeDrawElement | LinearElement;
