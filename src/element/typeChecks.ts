850bdfa3fef67016a0147e8658d4af4a1f15a39d
Jasper0077 <jasperhwong@hotmail.com>
2023-09-20 23:40:50 +0800

    add typechecks for element type

---
+++ src/element/typeChecks.ts
@@ -0,0 +1,28 @@
+import { AppState } from "../types";
+import { FreeDrawElement, Element, LinearElement } from "./types";
+
+export const isLinearElementType = (
+  elementType: AppState["activeTool"]["type"]["value"],
+): boolean => {
+  return (
+    elementType === "arrow" || elementType === "line" // || elementType === "freedraw"
+  );
+};
+
+export const isLinearElement = (
+  element?: Element | null,
+): element is LinearElement => {
+  return element != null && isLinearElementType(element.type);
+};
+
+export const isFreeDrawElement = (
+  element?: Element | null,
+): element is FreeDrawElement => {
+  return element != null && isFreeDrawElementType(element.type);
+};
+
+export const isFreeDrawElementType = (
+  elementType: Element["type"],
+): boolean => {
+  return elementType === "freedraw";
+};
