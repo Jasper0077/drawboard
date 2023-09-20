dec9f14b325bee9b93dfec279ff370a7d5028f10
Jasper0077 <jasperhwong@hotmail.com>
2023-09-20 23:39:46 +0800

    add random utils

---
+++ src/random.ts
@@ -0,0 +1,14 @@
+import { Random } from "roughjs/bin/math";
+import { nanoid } from "nanoid";
+
+let random = new Random(Date.now());
+let testIdBase = 0;
+
+export const randomInteger = () => Math.floor(random.next() * 2 ** 31);
+
+export const reseed = (seed: number) => {
+  random = new Random(seed);
+  testIdBase = 0;
+};
+
+export const randomId = () => nanoid();
